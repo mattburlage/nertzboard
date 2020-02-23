5c93cc994b49f75ebf6e17332f1e403a2714e980
matt burlage <mattburlage@gmail.com>
2020-02-23 01:16:15 -0500

    deployment

--- nertzboard/settings.py
+++ nertzboard/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'nertz.apps.NertzConfig',
     'rest_framework',
     'corsheaders',
-    'channels',
+    # 'channels',
 ]
 
 MIDDLEWARE = [
@@ -80,13 +80,23 @@ ASGI_APPLICATION = "nertzboard.routing.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mattburlage$nertzboard2',
+        'USER': 'mattburlage',
+        'PASSWORD': 'UUMh8TNq5A8mz9K',
+        'HOST': 'mattburlage.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
