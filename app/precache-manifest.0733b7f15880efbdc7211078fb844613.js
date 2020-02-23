e75ff174045fb3975b9cfd0eee236935341b02c2
matt burlage <mattburlage@gmail.com>
2020-02-23 01:55:25 -0500

    added react build

---
+++ app/precache-manifest.0733b7f15880efbdc7211078fb844613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ada74e10503767288eb1195fcb56e4",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "29520cc2f97a51282a3d",
+    "url": "/app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8ad4334e13775aafc287",
+    "url": "/app/static/css/main.72ec7ae9.chunk.css"
+  },
+  {
+    "revision": "29520cc2f97a51282a3d",
+    "url": "/app/static/js/2.34995daa.chunk.js"
+  },
+  {
+    "revision": "a2de51b82b64ff6d974aba76486f1bc9",
+    "url": "/app/static/js/2.34995daa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ad4334e13775aafc287",
+    "url": "/app/static/js/main.e9834fe1.chunk.js"
+  },
+  {
+    "revision": "56e7e180db9aaa18ac31",
+    "url": "/app/static/js/runtime-main.af24c34d.js"
+  }
+]);
