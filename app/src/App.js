e5e221a762b470be3f5346b629093f6e462dfb28
matt burlage <mattburlage@gmail.com>
2020-05-06 05:08:57 -0400

    added react build

--- app/src/App.js
+++ app/src/App.js
@@ -117,7 +117,7 @@ class App extends React.Component {
         })
             .then(res => res.json())
             .then(json => {
-                console.log(json)
+                // console.log(json)
                 // localStorage.setItem('token', json.token);
                 // this.setState({
                 //     has_key: !!localStorage.getItem('token'),
